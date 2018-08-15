800512b67632630a169d70a77aee788a76e78af2
ImAuty <teraringu@gmail.com>
2018-08-15 17:02:48 +0900

    脱出ゲーム用にメイン画面とステージ画面を作成

---
+++ app/src/main/java/aaa_sky/escapegame/Stage1Activity.java
@@ -0,0 +1,13 @@
+package aaa_sky.escapegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stage1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage1);
+    }
+}
