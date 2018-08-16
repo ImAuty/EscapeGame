0865d904857411d2e1f697d4e05051e95817b828
ImAuty <teraringu@gmail.com>
2018-08-16 20:50:24 +0900

    ステージ1のクリア状況をメイン画面に反映できるように処理を追加

--- app/src/main/java/aaa_sky/escapegame/MainActivity.java
+++ app/src/main/java/aaa_sky/escapegame/MainActivity.java
@@ -4,22 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CLEAR_STATE = "clear_state";
+    private static final int REQUEST_CODE = 0;
+    private boolean isClearStage1 = false;
+    private boolean isClearStage2 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showState();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && intent != null) {
+            isClearStage1 = intent.getBooleanExtra(Stage1Activity.CLEAR_STATE, isClearStage1);
+        }
+        showState();
+    }
+
+    public void showState() {
+        if (isClearStage1) {
+            ImageView imageView = findViewById(R.id.clearStateStage1);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if (isClearStage2) {
+            ImageView imageView = findViewById(R.id.clearStateStage2);
+            imageView.setVisibility(View.VISIBLE);
+        }
     }
 
     public void toStage1(View view) {
         Intent intent = new Intent(MainActivity.this, Stage1Activity.class);
-        super.startActivity(intent);
+        intent.putExtra(CLEAR_STATE, isClearStage1);
+        super.startActivityForResult(intent, REQUEST_CODE);
     }
 
     public void toStage2(View view) {
         Intent intent = new Intent(MainActivity.this, Stage2Activity.class);
-        super.startActivity(intent);
+        intent.putExtra(CLEAR_STATE, isClearStage2);
+        super.startActivityForResult(intent, REQUEST_CODE);
     }
 }
--- app/src/main/java/aaa_sky/escapegame/Stage1Activity.java
+++ app/src/main/java/aaa_sky/escapegame/Stage1Activity.java
@@ -1,18 +1,60 @@
 package aaa_sky.escapegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Stage1Activity extends AppCompatActivity {
+
+    public static final String CLEAR_STATE = "clear_state_stage1";
+    private int openCount = 0;
+    private boolean isClear = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage1);
+        Intent intent = super.getIntent();
+        isClear = intent.getBooleanExtra(MainActivity.CLEAR_STATE, false);
+    }
+
+    public void openDoor(View view) {
+        ImageView imageView = findViewById(R.id.doorImage);
+        switch (openCount) {
+            default:
+                imageView.setImageResource(R.drawable.door1);
+                openCount = 0;
+                break;
+            case 0:
+                imageView.setImageResource(R.drawable.door2);
+                openCount++;
+                break;
+            case 1:
+                imageView.setImageResource(R.drawable.door3);
+                openCount++;
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.door4);
+                isClear = true;
+                showMessage();
+                break;
+        }
+    }
+
+    public void showMessage() {
+        Toast toast = Toast.makeText(this, "Stage1 Clear", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 
     public void toMain(View view) {
+        Intent intent = new Intent();
+        intent.putExtra(CLEAR_STATE, isClear);
+        super.setResult(RESULT_OK, intent);
         super.finish();
     }
 }
